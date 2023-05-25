b4aab8cb3e31ad055b139b264c6d2298679886a5
Issah Mudasir <danladi496@gmail.com>
2023-05-25 07:25:49 +0000

    Updated-app-class

--- interfaces/app.rb
+++ interfaces/app.rb
@@ -1,158 +1,58 @@
-require_relative 'book'
-require_relative 'rental'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'person'
+require_relative '../components/book'
+require_relative '../components/rental'
+require_relative '../components/student'
+require_relative '../components/teacher'
+require_relative '../components/person'
+require_relative '../interfaces/books_ui'
+require_relative '../interfaces/person_ui'
+require_relative '../interfaces/rental_ui'
 
 class App
   attr_accessor :books, :people, :rentals
 
   def initialize
-    @books = []
-    @people = []
-    @rentals = []
+    @books = BookUserInterface.new
+    @people = PersonUserInterface.new
+    @rentals = RentalUserInterface.new(@books, @people)
+  end
+
+  def book_count
+    puts "\"#{@books.books.size}\" books available!"
+  end
+
+  def people_count
+    puts "\"#{@people.people.size}\" people in total!"
+  end
+
+  def rental_count
+    puts "\"#{@rentals.rentals.size}\" rental(s) made!"
   end
 
   def handle_student_input
-    age = handle_age
-    name = handle_name
-    parent_permission = handle_permission
-    student = Student.new(age, name, parent_permission: parent_permission, person_type: 'student')
-    @people << student
-    puts ['Person created succsefully', ' ']
-  end
-
-  def handle_age
-    print 'Age: '
-    gets.chomp.to_i
-  end
-
-  def handle_name
-    print 'Name: '
-    gets.chomp.capitalize
-  end
-
-  def handle_permission
-    print 'Has Parent permission? [Y/N]: '
-    parent_permission = gets.chomp.capitalize
-    case parent_permission
-    when 'Y'
-      true
-    when 'N'
-      false
-    else
-      puts 'Invalid selection, please type either Y or N'
-      handle_permission
-    end
+    @people.handle_student_input
   end
 
   def handle_teacher_input
-    age = handle_age
-    name = handle_name
-    specialization = handle_specialization
-    teacher = Teacher.new(age, specialization, name, person_type: 'teacher')
-    @people << teacher
-    puts ['Person created succsefully', ' ']
-  end
-
-  def handle_specialization
-    print 'Specialization: '
-    gets.chomp.capitalize
+    @people.handle_teacher_input
   end
 
   def list_all_people
-    @people.each do |person|
-      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-
-    puts ['No person on the list', ''] if @people.empty?
+    @people.list_all_people
   end
 
   def create_book
-    title = handle_book_title
-    author = handle_book_author
-    @books << Book.new(title, author)
-    puts 'Book created successfully'
-  end
-
-  def handle_book_title
-    print 'Title: '
-    gets.chomp.capitalize
-  end
-
-  def handle_book_author
-    print 'Author: '
-    gets.chomp.capitalize
+    @books.create_book
   end
 
   def list_all_books
-    @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author}"
-    end
-
-    puts ['No books found on the list', ''] if @books.empty?
+    @books.list_all_books
   end
 
   def create_rental
-    puts 'Select a book from the following list by number'
-    @books.each_with_index do |book, index|
-      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
-    end
-    book_number = handle_book_number
-    book = @books[book_number]
-
-    puts ['', 'Select a person from the following list by number (not id)']
-    @people.each_with_index do |person, index|
-      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
-    end
-
-    person_number = handle_person_number
-    person = @people[person_number]
-    puts ''
-
-    date = handle_date_input
-
-    @rentals << Rental.new(date, book, person)
-    puts 'Rental created successfully'
-  end
-
-  def handle_book_number
-    gets.chomp.to_i
-  end
-
-  def handle_person_number
-    gets.chomp.to_i
-  end
-
-  def handle_date_input
-    print 'Date: '
-    gets.chomp
+    @rentals.create_rental
   end
 
   def list_rentals_for_person
-    person_id = handle_person_input
-
-    person = @people.find { |p| p.id == person_id }
-    if person.nil?
-      puts "No person found with id #{person_id}"
-      return
-    end
-
-    puts 'Rentals: '
-
-    rentals = @rentals.select { |r| r.person.id == person_id }
-    if rentals.empty?
-      puts "No rentals found for person with id #{person_id}"
-      return
-    end
-
-    rentals.each do |rental|
-      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.person.name}"
-    end
-  end
-
-  def handle_person_input
-    print 'ID of person: '
-    gets.chomp.to_i
+    @rentals.list_rentals_for_person
   end
 end
