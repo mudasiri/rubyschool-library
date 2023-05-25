14b0647811eb7a3e6f0755618d369032033d96fa
Issah Mudasir <danladi496@gmail.com>
2023-05-25 07:30:21 +0000

    Refactored-teacher-tojson

--- components/teacher.rb
+++ components/teacher.rb
@@ -1,12 +1,17 @@
-require './person'
+require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true, person_type: 'teacher')
+  def initialize(age, name, specialization, parent_permission: true, person_type: 'teacher')
     super(age, name, parent_permission: parent_permission, person_type: person_type)
     @specialization = specialization
   end
 
   def can_use_services?
     true
+  end
+
+  def to_json(*_args)
+    { 'id' => @id, 'name' => @name, 'age' => @age, 'specialization' => @specialization, 'class' => self.class.name,
+      'rentals' => @rentals }
   end
 end
