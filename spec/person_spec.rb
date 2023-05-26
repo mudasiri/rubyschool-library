5b0a37ec3146d90ef1dbe5ed7c8fb0dafc202247
Issah Mudasir <danladi496@gmail.com>
2023-05-26 19:39:16 +0000

    Added-test-for-add-rental

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -10,6 +10,19 @@ describe Person do
       expect(@person).to be_an_instance_of Person
     end
 
+    it 'returns true for can_use_services?' do
+      expect(@person.can_use_services?).to eq(true)
+    end
+    describe '#add_rental' do
+    it "should create a new Rental object and add it to the person's rentals" do
+      book = Book.new('Title', 'Author')
+    date = '2023-5-25'
+    rental = Rental.new(date, book, @person)
+    @person.rent_book(rental)
+    expect(@person.rentals).to include(rental)
+    end
+  end
+
     it 'creates a new Person object with default values' do
       expect(@person.parent_permission).to eq(true)
       expect(@person.person_type).to eq('student')
