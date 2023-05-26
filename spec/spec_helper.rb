b4bd1195dd4e2e7631acbcebfbe5f6a7fa75d34c
Issah Mudasir <danladi496@gmail.com>
2023-05-26 01:17:08 +0000

    Created-a-commit-helper-file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require_relative '../components/book'
+require_relative '../components/classroom'
+require_relative '../components/person'
+require_relative '../components/rental'
+require_relative '../components/student'
+require_relative '../components/teacher'
+require_relative '../components/nameable'
+require_relative '../interfaces/books_ui'
+require_relative '../decorators/base_decorator'
+require_relative '../decorators/capitalize_decorator'
+require_relative '../decorators/trimmer_decorator'
+
+require 'json'
